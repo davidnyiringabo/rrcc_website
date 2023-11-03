4204055a1fc2b178b46ca11ac510f9e1181142a5
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-11-03 20:04:06 +0200

    .

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -37,13 +37,13 @@ const ActivitiesPage = ()=>{
     ]
     return(
         <div className="w-full flex flex-col justify-center px-4 md:px-14 py-10">
-            <header className="text-[180%] font-extrabold mb-5" >Our <span className=" text-[100%] border-b-[3px] border-red-800 text-green-700">Activities</span></header>
+            <h4 className="text-[180%] font-extrabold mb-5 mt-6">Our <span className="text-[100%] border-b-[3px] border-[#CC0000] text-green-700 hover:bg-[#CC0000] hover:p-1 hover:pb-0 hover:text-white hover:cursor-pointer">Activities</span></h4>
             <p className="w-full md:w-[50%] mb-2 my-3 mb-6 text-[80%] font-light text-[#1E1E1E]">The Red Cross carries out various activities to promote health, safety, and humanitarian values among students. Some of our activities include:</p>
             <p  className="w-full md:w-[50%] mb-2 my-3 text-[80%] font-light text-[#1E1E1E]">These programs provide opportunities for students to develop leadership skills, gain practical experience in community service, and contribute to the welfare of others. We aim to inspire compassion, empathy, and a sense of social responsibility among students, preparing them to be active and engaged members of their communities.</p>
             <SwapContainer/>
 
             <div>
-                <h4 className="text-[180%] mb-5 mt-7">Our Programs</h4>
+                <h4 className="text-[180%] mb-5 mt-7 font-extrabold">Our <span className="text-[100%] border-[#CC0000] border-b-[3px] hover:bg-[#CC0000] hover:p-1 hover:pb-0 hover:text-white hover:cursor-pointer">Programs</span></h4>
                 <div className="w-[95%] mx-auto flex flex-col md:grid grid-cols-3 gap-x-6 gap-y-6 items-center">
                     {programs.map((pro)=>{
                         return(
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,7 +5,7 @@ const Hero = ()=>{
     return(
         <div className="w-[100%] py-5 flex flex-col md:flex-row items-center md:items-start justify-between py-10 pt-10">
             <div className="w-full md:w-[50%]">
-                <h1 className="text-[180%] font-extrabold mb-5">Save a <span className="border-b-[3px] border-red-900 text-[100%] text-[#00A106] font-extrabold">Life</span> Today</h1>
+                <h1 className="text-[180%] font-extrabold mb-5">Save a <span className="border-b-[3px] border-red-900 text-[100%] text-[#00A106]  border-[#CC0000] text-green-700 hover:bg-[#CC0000] hover:p-1 hover:pb-0 hover:text-white hover:cursor-pointer font-extrabold">Life</span> Today</h1>
 
                 <p className="mb-2 my-3 mb-6 text-[80%] font-light text-[#1E1E1E]">The Red Cross is an international humanitarian organization that provides assistance and support to those in need, particularly during times of crisis and emergencies.</p>
                 <p  className="mb-2 my-3 text-[80%] font-light text-[#1E1E1E]">The mission of the Red Cross is to alleviate human suffering and protect human dignity. Its primary focus areas include disaster response, healthcare services, blood donation, and promoting international humanitarian law.</p>
