02d7da518c982f554611d162c5d11a600ed8d486
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-11-04 14:35:32 +0200

    feat: addactitivies page

---
+++ src/components/ImagesGroup/Goal/index.tsx
@@ -0,0 +1,8 @@
+import { StaticImageData } from "next/image"
+const GoalComponent = ({image, title, content}:{image: StaticImageData, title: string, content: string}) =>{
+    return(
+        <div className="border-[1px] border-[#CC0000] rounded-lg flex items-center justify-between ">
+
+        </div>
+    )
+}
